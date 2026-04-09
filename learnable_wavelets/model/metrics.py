58665d239f159a6c2e6e9da9006bdfe8ec833269
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2026-04-09 15:34:07 +0300

    fix: fix psnr, use torch.tensor for scalar

--- learnable_wavelets/model/metrics.py
+++ learnable_wavelets/model/metrics.py
@@ -3,6 +3,6 @@ import torch
 
 def psnr_metric(x_rec, x, range_: tuple[float, float] = (-1.0, 1.0)):
     mse = torch.mean((x_rec - x) ** 2, dim=[1, 2])
-    max_i = range_[1] - range_[0]
+    max_i = torch.tensor(range_[1] - range_[0], device=x.device, dtype=x.dtype)
     psnr = 20 * torch.log10(max_i) - 10 * torch.log10(mse)
     return torch.mean(psnr)
