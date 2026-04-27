fix: fix psnr computation

learnable_wavelets/model/metrics.py
@@ -6,9 +6,13 @@ def psnr_metric(
 ):
     if loss is not None:
         mse = loss
+        device = loss.device
+        dtype = loss.dtype
     else:
         mse = torch.mean((x_rec - x) ** 2, dim=[1, 2])
+        device = x.device
+        dtype = x.dtype
 
-    max_i = torch.tensor(range_[1] - range_[0], device=x.device, dtype=x.dtype)
+    max_i = torch.tensor(range_[1] - range_[0], device=device, dtype=dtype)
     psnr = 20 * torch.log10(max_i) - 10 * torch.log10(mse)
     return torch.mean(psnr)