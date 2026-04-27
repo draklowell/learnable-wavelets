a5b08d9b08cc48c23ac9b57205ee9cfed7818acb
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2026-04-27 16:28:46 +0300

    fix: psnr

--- learnable_wavelets/model/metrics.py
+++ learnable_wavelets/model/metrics.py
@@ -5,14 +5,10 @@ def psnr_metric(
     x_rec=None, x=None, loss=None, range_: tuple[float, float] = (-1.0, 1.0)
 ):
     if loss is not None:
-        mse = loss
-        device = "cpu"
-        dtype = torch.float32
+        mse = torch.tensor(loss, device="cpu", dtype=torch.float32)
     else:
         mse = torch.mean((x_rec - x) ** 2, dim=[1, 2])
-        device = x.device
-        dtype = x.dtype
 
-    max_i = torch.tensor(range_[1] - range_[0], device=device, dtype=dtype)
+    max_i = torch.tensor(range_[1] - range_[0], device=mse.device, dtype=mse.dtype)
     psnr = 20 * torch.log10(max_i) - 10 * torch.log10(mse)
     return torch.mean(psnr)
